866f08d3d4e50b49951b706ab6367fd2ab642fa8
Mahesh babu <gmahesh007babu@gmail.com>
2025-10-18 03:02:28 +0000

    resolve ZIP, fetch weather, cache by ZIP for 30m; show cache badge

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,6 +1,26 @@
 class ForecastsController < ApplicationController
   def index
-    # Only the form for now; wiring comes later.
     @address = params[:address].to_s.strip.presence
+    return unless @address
+
+    zip = Geocoding::GeocodeAddress.call(@address)
+    unless zip
+      flash.now[:alert] = "Could not resolve ZIP for that address."
+      return
+    end
+
+    cache_key = "forecast:zip:#{zip}"
+    from_cache = true
+    data = Rails.cache.fetch(cache_key, expires_in: 30.minutes) do
+      from_cache = false
+      Weather::FetchForecast.call(zip)
+    end
+
+    unless data
+      flash.now[:alert] = "No forecast available."
+      return
+    end
+
+    @result = data.merge(zip:, from_cache:)
   end
 end
